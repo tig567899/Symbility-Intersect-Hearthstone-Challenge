eaaafa7401e3f75ebc9f41682764f1cdcbd4fe69
Tiger Kong <tig567899@users.noreply.github.com>
2019-02-06 23:40:23 -0500

    Finished filter capability

--- app/src/main/java/tig567899/symbilitychallenge/GridActivity.java
+++ app/src/main/java/tig567899/symbilitychallenge/GridActivity.java
@@ -1,54 +1,162 @@
 package tig567899.symbilitychallenge;
 
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class GridActivity extends AppCompatActivity implements ApiConnector.ResponseInterface {
+public class GridActivity extends AppCompatActivity implements ApiConnector.ResponseInterface, GridAdapter.QueryDeletionInterface {
 
+    private static final String TAG = "GridActivity";
     ProgressBar progressBar;
     RecyclerView recyclerView;
     GridAdapter gridAdapter;
+    ImageView search_btn;
+    ArrayList<String> queries;
+    ArrayList<Card> cards;
+    ArrayList<Card> searched_cards;
+    boolean searched = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
 
         progressBar = findViewById(R.id.loading);
 
         recyclerView = findViewById(R.id.card_grid);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle(R.string.cards);
+        search_btn = findViewById(R.id.search_icon);
+        search_btn.setOnClickListener(view -> {
+            if (searched)
+            {
+                showSearchDialog();
+            }
+        });
         ApiConnector.get(this);
     }
 
     @Override
     public void onSuccess() {
-        ArrayList<Card> cards = (ArrayList<Card>) ApiConnector.getData();
+        cards = (ArrayList<Card>) ApiConnector.getData();
+        searched_cards = new ArrayList<>();
+        searched_cards.addAll(cards);
         Toast.makeText(this, "Successful Connection " + cards.size(), Toast.LENGTH_SHORT).show();
         progressBar.setVisibility(View.GONE);
+        queries = new ArrayList<>();
+        searched = true;
 
-        gridAdapter = new GridAdapter(cards, this);
+        gridAdapter = new GridAdapter(searched_cards, queries, this, this);
+        gridAdapter.setHasStableIds(true);
         recyclerView.setAdapter(gridAdapter);
     }
 
     @Override
     public void onFailure() {
         Toast.makeText(this, "Failed Connection", Toast.LENGTH_SHORT).show();
+        progressBar.setVisibility(View.GONE);
     }
 
     @Override
     public void onBackPressed() {
-        // Do nothing
+        if (searched)
+        {
+            searched_cards.clear();
+            searched_cards.addAll(cards);
+            Log.d(TAG, "Searched cards size: " + searched_cards.size());
+
+            gridAdapter.notifyDataSetChanged();
+            queries.clear();
+        }
+
+    }
+
+    private void showSearchDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Search");
+        EditText edit = new EditText(this);
+        builder.setView(edit);
+
+        builder.setPositiveButton("Search", (dialogInterface, i) -> {
+            String query = edit.getText().toString();
+            for (int t = 0; t < queries.size(); ++t)
+            {
+                if (queries.get(t).equals(query))
+                {
+                    dialogInterface.dismiss();
+                    return;
+                }
+            }
+            queries.add(query);
+            filterQueries();
+            dialogInterface.dismiss();
+        });
+
+        builder.setNegativeButton("Cancel", (dialogInterface, i) ->
+        {
+            dialogInterface.dismiss();
+        });
+        builder.show();
+    }
+
+    private void filterQueries()
+    {
+        searched_cards.clear();
+        searched_cards.addAll(cards);
+        for (int i = 0; i < queries.size(); ++i)
+        {
+            String query = queries.get(i);
+            ArrayList<Card> tempsearch = new ArrayList<>();
+            Log.d(TAG, "Query: " + query);
+            for (int x = 0; x < searched_cards.size(); ++x)
+            {
+                int querysize = query.length();
+                for (int y = 0; y < searched_cards.get(x).getName().length(); ++y)
+                {
+                    if (y + querysize > searched_cards.get(x).getName().length())
+                    {
+                        break;
+                    }
+                    if (searched_cards.get(x).getName().substring(y, y+querysize).toLowerCase().equals(query.toLowerCase()))
+                    {
+                        tempsearch.add(searched_cards.get(x));
+                    }
+                }
+            }
+            Log.d(TAG, "Searched cards size: " + tempsearch.size());
+            searched_cards.clear();
+            searched_cards.addAll(tempsearch);
+        }
+        gridAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onDelete(String item) {
+        for (int i = 0; i < queries.size(); ++i)
+        {
+            if (queries.get(i).toLowerCase().equals(item.toLowerCase()))
+            {
+                queries.remove(i);
+                filterQueries();
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/tig567899/symbilitychallenge/GridAdapter.java
+++ app/src/main/java/tig567899/symbilitychallenge/GridAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,52 +23,75 @@ import java.util.ArrayList;
 
 public class GridAdapter extends RecyclerView.Adapter {
 
-    ArrayList<Card> cards;
-    Context ctx;
-    public GridAdapter(ArrayList<Card> cards, Context ctx)
+    private static final String TAG = "GridAdapter";
+    private ArrayList<Card> cards;
+    private ArrayList<String> queries;
+    private Context ctx;
+    private QueryDeletionInterface qdi;
+
+    public GridAdapter(ArrayList<Card> cards, ArrayList<String> queries, Context ctx, QueryDeletionInterface qdi)
     {
         this.ctx = ctx;
+        this.queries = queries;
         this.cards = cards;
+        this.qdi = qdi;
     }
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(ctx).inflate(R.layout.grid_item, viewGroup, false);
-        return new CardHolder(v);
+    public GenericHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v;
+        if (i == 0)
+        {
+            v = LayoutInflater.from(ctx).inflate(R.layout.query_item, viewGroup, false);
+            return new QueryHolder(v);
+        }
+        else if (i == 1)
+        {
+            v = LayoutInflater.from(ctx).inflate(R.layout.blank_item, viewGroup, false);
+            return new BlankHolder(v);
+        }
+        else
+        {
+            v = LayoutInflater.from(ctx).inflate(R.layout.grid_item, viewGroup, false);
+            return new CardHolder(v);
+        }
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-        CardHolder ch = (CardHolder)viewHolder;
-        Card c = cards.get(i);
-        if (c.getImgurl() == null || c.getImgurl().equals(""))
+        GenericHolder holder = (GenericHolder) viewHolder;
+        holder.bindViews(i);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position < queries.size())
         {
-            Glide.with(ctx)
-                    .load(R.drawable.placeholder)
-                    .into(ch.img);
+            return 0;
+        }
+        else if (position == queries.size() && position %2 == 1)
+        {
+            return 1;
         }
         else
         {
-            RequestOptions options = new RequestOptions()
-                    .placeholder(R.drawable.placeholder)
-                    .error(R.drawable.placeholder);
-            Glide.with(ctx)
-                    .load(c.getImgurl())
-                    .apply(options)
-                    .into(ch.img);
+            return 2;
         }
-
-        ch.playerClass.setText(c.getPlayerClass());
-        ch.name.setText(c.getName());
-        ch.type.setText(c.getType());
     }
 
     @Override
     public int getItemCount() {
-        return cards.size();
+        return cards.size() + queries.size();
     }
 
-    public class CardHolder extends RecyclerView.ViewHolder
+    public class GenericHolder extends RecyclerView.ViewHolder {
+        public GenericHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+        public void bindViews(int position){};
+    }
+
+    public class CardHolder extends GenericHolder
     {
         public ImageView img;
         public TextView name;
@@ -80,5 +104,68 @@ public class GridAdapter extends RecyclerView.Adapter {
             type = itemView.findViewById(R.id.type);
             playerClass = itemView.findViewById(R.id.player_class);
         }
+
+        @Override
+        public void bindViews(int pos) {
+            int i = pos-queries.size();
+            if (queries.size() %2 == 1)
+            {
+                i--;
+            }
+            super.bindViews(i);
+            Card c = cards.get(i);
+            if (c.getImgurl() == null || c.getImgurl().equals(""))
+            {
+                Glide.with(ctx)
+                        .load(R.drawable.placeholder)
+                        .into(img);
+            }
+            else
+            {
+                RequestOptions options = new RequestOptions()
+                        .placeholder(R.drawable.placeholder)
+                        .error(R.drawable.placeholder);
+                Glide.with(ctx)
+                        .load(c.getImgurl())
+                        .apply(options)
+                        .into(img);
+            }
+
+            playerClass.setText(c.getPlayerClass());
+            name.setText(c.getName());
+            type.setText(c.getType());
+        }
+    }
+
+    public class QueryHolder extends GenericHolder{
+        TextView query;
+        ImageView clear_button;
+        public QueryHolder(@NonNull View itemView) {
+            super(itemView);
+            query = itemView.findViewById(R.id.query_text);
+            clear_button = itemView.findViewById(R.id.clear);
+
+            clear_button.setOnClickListener(view -> {
+                qdi.onDelete(query.getText().toString());
+            });
+        }
+
+        @Override
+        public void bindViews(int position) {
+            super.bindViews(position);
+            query.setText(queries.get(position));
+        }
+    }
+
+    public class BlankHolder extends GenericHolder
+    {
+        BlankHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public interface QueryDeletionInterface
+    {
+        void onDelete(String item);
     }
 }
